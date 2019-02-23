2d4b81c63dabed73c14bb82a47ffc5556f778f4b
Alexis ROLLAND <alexisrolland@hotmail.com>
2019-02-23 20:30:20 +0800

    Intermediate commit

--- api/init/proxy/routes.py
+++ api/init/proxy/routes.py
@@ -1,12 +1,8 @@
-from docker.errors import APIError
 from flask import request, jsonify, make_response
 from flask_restplus import Resource, fields, Namespace, Api
 from proxy.exceptions import RequestException
-from proxy.interceptor import Interceptor
 from proxy.utils import validate_graphql_request, execute_graphql_request
 from security.decorators import token_required
-
-# pylint: disable=unused-variable
 
 
 def register_graphql(namespace: Namespace, api: Api):
@@ -33,30 +29,12 @@ def register_graphql(namespace: Namespace, api: Api):
             payload = request.json
 
             try:
-                # Validate http request payload and convert it to GraphQL document
-                graphql_document = validate_graphql_request(payload['query'])
-
-                # Verify GraphQL mutation can be handled
-                interceptor = Interceptor()
-                mutation_name = interceptor.get_mutation_name(graphql_document)
-
-                # Surcharge payload before request
-                if mutation_name:
-                    payload['query'] = interceptor.before_request(mutation_name)
-
                 # Execute request on GraphQL API
                 status, data = execute_graphql_request(payload)
                 if status != 200:
                     raise RequestException(status, data)
 
-                # Execute custom scripts after request
-                if mutation_name:
-                    data = interceptor.after_request(mutation_name, data)
-
                 return make_response(jsonify(data), status)
 
             except RequestException as exception:
                 return exception.to_response()
-
-            except APIError as exception:
-                return make_response(jsonify({'message': exception.explanation}), exception.status_code)
--- api/init/security/routes.py
+++ api/init/security/routes.py
@@ -1,9 +1,42 @@
-from flask_restplus import Namespace
-from security.oauth.github import register_github_oauth
-from security.oauth.google import register_google_oauth
+from flask import jsonify, make_response
+from flask_restplus import Resource, Namespace, Api
+from proxy.utils import validate_graphql_request, execute_graphql_request
+from proxy.exceptions import RequestException
 
 
-def register_security(namespace: Namespace):
-    """Registers all resources for the security module"""
-    register_github_oauth(namespace)
-    register_google_oauth(namespace)
+def register_security(namespace: Namespace, api: Api):
+    """Method used to register the security namespace and endpoints."""
+
+    # Create expected headers and payload
+    headers = api.parser()
+    headers.add_argument('login', type=str, location='headers', help='User login')
+    headers.add_argument('password', type=str, location='headers', help='User password')
+
+    @namespace.route('/token')
+    @namespace.doc()
+    class Token(Resource):
+
+        @namespace.expect(headers, validate=True)
+        def get(self):
+            """
+            Get token
+            Use this endpoint to get the token in exchange from a valid user login and password.
+            """
+
+            # Build payload of http request
+            args = headers.parse_args()
+            payload = {}
+            payload['query'] = "mutation authenticateUser($userEmail: String, $userPassword: String) { authenticateUser(input: { userEmail: $userEmail userPassword: $userPassword }) { sysUser { id email role flagActive }}}"
+            payload['variables']['userEmail'] = args.login
+            payload['variables']['userPassword'] = args.password
+
+            try:
+                # Execute request on GraphQL API
+                status, data = execute_graphql_request(payload)
+                if status != 200:
+                    raise RequestException(status, data)
+
+                return make_response(jsonify(data), status)
+
+            except RequestException as exception:
+                return exception.to_response()
