cc3781b90927bff68b49157ad50d9eaf072e4d77
Alexis ROLLAND <alexisrolland@hotmail.com>
2019-02-24 21:05:26 +0800

    Delete Flask API component and remove unused files

--- test/run-tests.sh
+++
@@ -1,6 +0,0 @@
-# Build images
-cd ..
-docker-compose -f docker-compose.yml -f docker-compose.test.yml build
-
-# Run test on all backend modules
-docker-compose -f docker-compose.yml -f docker-compose.test.yml up test-db test-api
