3a04d22ae31089d256cdbffadfac4a9d58cf63ca
Alexis Rolland <alexis.rolland@ubisoft.com>
2019-04-06 17:20:41 +0800

    make data type and linked attribute readonly when updating existing attribute

--- app/init/src/components/attribute/AttributeSelectAttribute.vue
+++ app/init/src/components/attribute/AttributeSelectAttribute.vue
@@ -1,13 +1,20 @@
 <template>
-    <div class="form-group">
+    <div v-if="showTreeSelect" class="form-group">
         <label for="linkedAttribute" class="col-form-label">
-            Link With List:
+            Link with List:
         </label>
         <treeselect placeholder="Select list attribute"
             v-model="selectedValue"
             v-bind:options="options"
             v-bind:multiple="false"
             v-bind:disable-branch-nodes="true"/>
+    </div>
+    <div v-else class="form-group">
+        Linked with List: 
+        <router-link v-bind:to="'/lists/' + linkedAttribute.listId">
+            {{ linkedAttribute.sysListByListId.name }} 
+            ({{ linkedAttribute.name }})
+        </router-link>
     </div>
 </template>
 
@@ -20,12 +27,22 @@ export default {
         'treeselect': Treeselect
     },
     props: {
-        value: Number
+        value: Number,
+        linkedAttribute: Object
     },
     data() {
         return {
             'selectedValue': this.value,
             'options': []
+        }
+    },
+    computed: {
+        attributeId() {
+            return this.$route.params.attributeId;
+        },
+        showTreeSelect() {
+            if(this.attributeId == 'new'){ return true; }
+            else { return false; }
         }
     },
     watch: {
@@ -37,25 +54,28 @@ export default {
         }
     },
     created: function () {
-        let payload = { 'query': this.$store.state.queryGetLinkedLists };
-        let headers = {};
-        if (this.$session.exists()) {
-            headers = { 'Authorization': 'Bearer ' + this.$session.get('jwt') };
-        };
-        this.$http.post(this.$store.state.graphqlUrl, payload, {headers}).then (
-            function(response){
-                if(response.data.errors){
-                    this.$store.state.errorObject.flag = true;
-                    this.$store.state.errorObject.message = response.data.errors[0].message;
-                } else {
-                    this.options = response.data.data.allSysLists.nodes;
-                    for (let i = 0; i < this.options.length; i++) {
-                        this.options[i]['children'] = this.options[i]['attributes']['children'];
-                        delete this.options[i]['attributes'];
+        //Only query list of linked list if it's a new attribute
+        if(this.attributeId == 'new'){
+            let payload = { 'query': this.$store.state.queryGetLinkedLists };
+            let headers = {};
+            if (this.$session.exists()) {
+                headers = { 'Authorization': 'Bearer ' + this.$session.get('jwt') };
+            };
+            this.$http.post(this.$store.state.graphqlUrl, payload, {headers}).then (
+                function(response){
+                    if(response.data.errors){
+                        this.$store.state.errorObject.flag = true;
+                        this.$store.state.errorObject.message = response.data.errors[0].message;
+                    } else {
+                        this.options = response.data.data.allSysLists.nodes;
+                        for (let i = 0; i < this.options.length; i++) {
+                            this.options[i]['children'] = this.options[i]['attributes']['children'];
+                            delete this.options[i]['attributes'];
+                        }
                     }
                 }
-            }
-        );
+            );
+        }
     }
 }
 </script>
