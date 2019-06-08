60799ad0b97ca40d2294d3413388d4232adf3070
alexisrolland <alexisrolland@hotmail.com>
2019-06-08 15:42:50 +0800

    Upgrade to Vue CLI v3

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,9 +5,13 @@ then
     # Minify Javascript files and create distribution folder (dist)
     npm run build
     
-    # Delete all unnecessary files except dist folder and index.html
-    rm -rf ./libs
+    # Move files to app root folder
+    cp ./dist/* .
+
+    # Delete all unnecessary files
+    rm -rf ./dist
     rm -rf ./node_modules
+    rm -rf ./public
     rm -rf ./src
 
     # Serve files from current folder /srv/app
@@ -16,5 +20,5 @@ then
 # Serve app for development
 else
     # Run app in development mode
-    npm run dev
+    npm run serve
 fi
