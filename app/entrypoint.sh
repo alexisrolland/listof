39b697a2320f581f1b816d3f9949e0e2f1e385db
alexisrolland <alexisrolland@hotmail.com>
2019-06-08 18:55:18 +0800

    Finalize migration to Vue CLI v3

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,11 +2,13 @@
 # Build app and serve it for production
 if [ "$1" = "prod" ]
 then
+    export NODE_ENV=production
+
     # Minify Javascript files and create distribution folder (dist)
     npm run build
-    
+
     # Move files to app root folder
-    cp ./dist/* .
+    cp -a ./dist/. ./
 
     # Delete all unnecessary files
     rm -rf ./dist
@@ -19,6 +21,8 @@ then
 
 # Serve app for development
 else
+    export NODE_ENV=development
+
     # Run app in development mode
     npm run serve
 fi
